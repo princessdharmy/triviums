3dea67cd1ef3762b294cdb71cc7c186f6bc53b90
damomos <damomos123@gmail.com>
2018-08-20 12:05:58 +0100

    Removed unwanted imported classes

--- app/src/main/java/com/app/horizon/screens/main/home/stages/StagesFragment.java
+++ app/src/main/java/com/app/horizon/screens/main/home/stages/StagesFragment.java
@@ -9,12 +9,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.app.horizon.R;
 import com.app.horizon.core.base.BaseFragment;
